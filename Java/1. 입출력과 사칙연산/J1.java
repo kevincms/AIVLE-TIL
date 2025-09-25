26b702d18053a46d7f7f98e1314d757aa39a64d3
kevincms <kevincms314@naver.com>
2025-09-25 21:50:02 +0900

    Buffer을 이용한 출력속도 향상 (해당 문제에서는 큰 변화가 없었음 추후 확인 필요)

--- Java/1. 입출력과 사칙연산/J1.java
+++ Java/1. 입출력과 사칙연산/J1.java
@@ -1,5 +1,23 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class J1 {
+    // Buffer를 이용해 입출력을 할 경우 main 함수에 throws Exception를 추가해 예외처리를 해주어야 한다.
+    public static void main(String[] args) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("Hello World!");
+
+        bw.flush();
+        bw.close();
+    }
+    
+}
+
+/*
 public class J1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
+
+ */

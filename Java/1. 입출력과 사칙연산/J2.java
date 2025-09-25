2cab963a829c18c16a48271c1482b9d81738e8c3
kevincms <kevincms314@naver.com>
2025-09-25 21:51:49 +0900

    next + 자료형이 아닌 문자열로 입력 받은 후 자료형 변환

--- Java/1. 입출력과 사칙연산/J2.java
+++ Java/1. 입출력과 사칙연산/J2.java
@@ -4,8 +4,23 @@ public class J2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a, b;
-        a=sc.nextInt();
-        b=sc.nextInt();
+
+        // case 1
+        // next + 자료형으로 입력받기
+
+        // a=sc.nextInt();
+        // b=sc.nextInt();
+
+        // case 2
+        // 자료형.parse자료형()으로 자료형을 바꿀 수 있음.
+
+        String input1, input2;
+        input1=sc.next();
+        input2=sc.next();
+        a=Integer.parseInt(input1);
+        b=Integer.parseInt(input2);
+
         System.out.println(a+b);
     }
 }
+

bc70651d75970f68bb396c523f648f16fe3c6cca
kevincms <kevincms314@naver.com>
2025-09-27 23:53:11 +0900

    print에 문자열이 아닌 정수 입력 가능

---
+++ Java/1. 입출력과 사칙연산/J8.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class J8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int diff, input;
+        diff=2541-1998;
+        input=sc.nextInt();
+        System.out.print(input-diff);
+    }
+}

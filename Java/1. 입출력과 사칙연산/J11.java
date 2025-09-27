d82c0c82161b1c15dc54df4602d546e22ec5a4b7
kevincms <kevincms314@naver.com>
2025-09-28 00:01:40 +0900

    int로 부족해서 long으로 자료형 수정. next+자료형도 같이 바꿔줘야 함.

---
+++ Java/1. 입출력과 사칙연산/J11.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class J11 {
+    public static void main(String[] args) {
+        Scanner st = new Scanner(System.in) ;
+        long a, b, c;
+        a=st.nextLong();
+        b=st.nextLong();
+        c=st.nextLong();
+        System.out.println(a+b+c);
+    }
+}

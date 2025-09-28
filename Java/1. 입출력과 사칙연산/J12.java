806d37a1c12941b9d5c9a97e378d8f67ca0a3869
kevincms <kevincms314@naver.com>
2025-09-28 23:21:19 +0900

    고양이랑 개 출력 이스케이프는 C 계열과 똑같음

---
+++ Java/1. 입출력과 사칙연산/J12.java
@@ -0,0 +1,11 @@
+public class J1 {
+    public static void main(String[] args) {
+        String cat="""
+                \\    /\\
+                 )  ( \')
+                (  /  )
+                 \\(__)|
+                """;
+        System.out.print(cat);
+    }
+}
---
+++ Java/1. 입출력과 사칙연산/J13.java
@@ -0,0 +1,12 @@
+public class J13 {
+    public static void main(String[] args) {
+        String dog="""
+                |\\_/|
+                |q p|   /}
+                ( 0 )\"\"\"\\
+                |\"^\"`    |
+                ||_/=\\\\__|
+                """;
+        System.out.println(dog);
+    }
+}

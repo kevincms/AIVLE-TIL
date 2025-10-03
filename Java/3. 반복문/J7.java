0fca447c1ba75091ee8b9f26c0630a0f6fcfcf9e
kevincms <kevincms314@naver.com>
2025-10-03 23:37:33 +0900

    java - 간단 반복문 입력 buffer + StreamTokenizer, 출력 StringBuilder 현재까지 발견한 것 중 가장 빠른듯

---
+++ Java/3. 반복문/J7.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class J7 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer st = new StreamTokenizer(br);
+        StringBuilder sb = new StringBuilder();
+        int T, a, b;
+        st.nextToken(); T=(int)st.nval;
+        for (int i = 1; i < T+1; i++) {
+            st.nextToken(); a=(int)st.nval;
+            st.nextToken(); b=(int)st.nval;
+            sb.append("Case #%d: %d\n".formatted(i,a+b));
+        }
+        System.out.print(sb);
+    }
+}

99a72c599d43d3c95508cab4945604cce84978d7
kevincms <kevincms314@naver.com>
2025-10-03 23:34:37 +0900

    java - 반복문 split 대신 StreamTokenizer를 사용하는게 속도가 더 빠름.

--- Java/3. 반복문/J6.java
+++ Java/3. 반복문/J6.java
@@ -1,30 +1,27 @@
 // https://velog.io/@xav/Java-String-StringBuffer-StringBuilder
 
 import java.io.*;
-import java.util.Scanner;
 
 public class J6 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StreamTokenizer st = new StreamTokenizer(br);
+        StringBuilder sb = new StringBuilder();
+
+        st.nextToken();
         int n, a, b;
-        n = Integer.parseInt(br.readLine());
-        String[] str;
+        n = (int) st.nval;
         for (int i = 0; i < n; i++) {
-            str=br.readLine().split(" ");
-            a = Integer.parseInt(str[0]);
-            b = Integer.parseInt(str[1]);
-            bw.write(String.valueOf(a+b)+"\n");
+            st.nextToken(); a = (int) st.nval;
+            st.nextToken(); b = (int) st.nval;
+            sb.append(a + b).append("\n");
         }
-        br.close();
-        bw.flush();
-        bw.close();
+        System.out.print(sb);
     }
 }
 
-/*
+/* 입력 buffer, 출력 StringBuilder
 import java.io.*;
-import java.util.Scanner;
 
 public class J6 {
     public static void main(String[] args) throws IOException {
@@ -44,7 +41,31 @@ public class J6 {
     }
 }
  */
-/*
+
+/* 입력 buffer, 출력 buffer
+import java.io.*;
+
+public class J6 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n, a, b;
+        n = Integer.parseInt(br.readLine());
+        String[] str;
+        for (int i = 0; i < n; i++) {
+            str=br.readLine().split(" ");
+            a = Integer.parseInt(str[0]);
+            b = Integer.parseInt(str[1]);
+            bw.write(String.valueOf(a+b)+"\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
+ */
+
+/* 입력 Scanner
 import java.util.Scanner;
 
 public class J6 {
